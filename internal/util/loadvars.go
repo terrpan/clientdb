d45717bd549e42d420867129261463abf30bd8fa
Daniel Terry <daniel.terry@madepeople.se>
2022-02-20 12:03:50 +0100

    go fmt

--- internal/util/loadvars.go
+++ internal/util/loadvars.go
@@ -1,29 +1,29 @@
 package util
 
 import (
-  "context"
-  "os"
+	"context"
+	"os"
 
-  "go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 const (
-  VarPrefix = "CLIENTDB_"
+	VarPrefix = "CLIENTDB_"
 )
 
 var (
-  MongoDBHost               = GetEnv(VarPrefix+"MONGODB_HOST", "localhost")
-  MongoDBPort               = GetEnv(VarPrefix+"MONGODB_PORT", "27017")
-  MongoDBName               = GetEnv(VarPrefix+"MONGODB_NAME", "test")
-  LogLevel                  = GetEnv(VarPrefix+"LOG_LEVEL", "info")
-  ctx                       = context.TODO()
-  DB          *mongo.Client = DbConnect()
+	MongoDBHost               = GetEnv(VarPrefix+"MONGODB_HOST", "localhost")
+	MongoDBPort               = GetEnv(VarPrefix+"MONGODB_PORT", "27017")
+	MongoDBName               = GetEnv(VarPrefix+"MONGODB_NAME", "test")
+	LogLevel                  = GetEnv(VarPrefix+"LOG_LEVEL", "info")
+	ctx                       = context.TODO()
+	DB          *mongo.Client = DbConnect()
 )
 
 // func GetEnv is a function to get environment variables to be able to set default values if not set
 func GetEnv(key, fallback string) string {
-  if value, ok := os.LookupEnv(key); ok {
-    return value
-  }
-  return fallback
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
 }
